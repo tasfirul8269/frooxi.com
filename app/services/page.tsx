86a47422b77388cb4b4d282b64c14e63829098f4
Tasfirul Haque <adef05675@gmail.com>
2025-07-20 21:24:49 +0600

    first commit

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import ServiceImage from '../../public/ServiceImage.jng';
+import ServiceImage from '../../public/ServiceImage.png';
 
 export default function Services() {
   return (
